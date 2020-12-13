ccc14f30b38f85b5232eeaa1ca165f8c16ddd211
calmpsycho333 <wrathking75@yahoo.com>
2020-12-12 20:03:55 -0500

    commit tree b7386989dc38b6e66bdc2199fc32ba3427d84978

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def oxford_comma(array)
+    #whats next?
+   if array.length <= 2
+    #whats next?
+    return array.join (" and ") 
+    #and?
+   elsif array.length > 2
+    #and?
+    empty = array.last
+   string = "and "
+    string << empty
+    array.pop
+    array << string
+   # binding.pry
 
+    return array.join(", ")
+   end
 end
